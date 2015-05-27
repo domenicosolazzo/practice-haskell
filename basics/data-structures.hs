1cb388e7b76ae9edbcabe027c92a00fe2b1f66a8
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-27 07:43:25 +0200

    Adding data structures in haskell: list and tuple

---
+++ basics/data-structures.hs
@@ -0,0 +1,12 @@
+-- List
+n = [1, 2, 3] -- List holds multiple values of the same type
+b = [True, False, True]
+c = ['H', 'e', 'l', 'l', 'o']
+
+arr = 'A': ['B', 'C', 'D'] -- : operator append a value at the beginning of the list 'ABCD'
+
+-- Tuples: They hold a fixed number of values, which can have different types
+tp = (1, True)
+
+z = zip [1 .. 5] ['a' .. 'e']
+main = print z
