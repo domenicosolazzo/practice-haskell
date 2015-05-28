036ee82479d8091e39dc2c2e4fa898b441aa4207
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-28 08:15:18 +0200

    Algebraic data types and operations

---
+++ basics/algebraic_datatype.hs
@@ -0,0 +1,10 @@
+-- Type where we specify the shape of each of the elements
+
+-- Algebraic operations 
+-- "sum" is alternation (A | B, meaning A or B but not both)
+-- "product" is combination (A B, meaning A and B together)
+data Pair = P Int Double -- Pair of numbers, an Int and a Double
+data Pair = I Int | D Double -- Either an Int or a double
+
+
+
