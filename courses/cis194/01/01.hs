88f2824f1cc4a7af78d01c08c3bfa0de0adc5707
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-05-29 22:19:06 +0200

    Basic types

--- courses/cis194/01/01.hs
+++ courses/cis194/01/01.hs
@@ -1,3 +1,41 @@
 -- Declaration and variables
-x::Int
+x :: Int
 x = 3
+
+-- Basic Types
+i :: Int
+i = 78
+
+biggestInt, smallestInt :: Int
+biggestInt = maxBound
+smallestInt = minBound
+
+-- Arbitrary-precision integers (limited by the amount of memory on your machine)
+n :: Integer
+n = 1234567890987654321987340982334987349872349874534
+
+reallyBig :: Integer
+reallyBig = 2^(2^(2^(2^2)))
+
+numDigits :: Int
+numDigits = length (show reallyBig)
+
+-- Double-precision floating point
+d1, d2 :: Double
+d1 = 4.5387
+d2 = 6.2831e-4
+
+-- Booleans
+b1, b2 :: Bool
+b1 = True
+b2 = False
+
+-- Unicode characters
+c1, c2, c3 :: Char
+c1 = 'x'
+c2 = 'Ø'
+c3 = 'ダ'
+
+-- Strings are lists of characters
+s :: String
+s = "Hello, Haskell!"
